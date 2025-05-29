1a6037a339739af1cac550f9aaa53fbacb2c737e
Jmailh18 <149411414+Jmailh18@users.noreply.github.com>
2025-05-29 15:41:04 -0500

    Create block_letters.py

---
+++ block_letters.py
@@ -0,0 +1,23 @@
+#Jake M
+#I like trains
+block_letter = """
+JJJJJ
+  J
+  J
+  J
+J J
+J J
+JJ
+"""
+print(block_letter)
+
+block_letter2 = """
+M   M
+MM MM
+MM MM
+M M M
+M   M
+M   M
+M   M
+"""
+print(block_letter2)
